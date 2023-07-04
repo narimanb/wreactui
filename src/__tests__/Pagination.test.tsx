dff2734b331ceb887658c68f48e2071471be3c3f
narimanb <nariman.bortov@gmail.com>
2023-07-04 05:12:31 +0200

    test: add NavigationButton test

--- src/__tests__/Pagination.test.tsx
+++ src/__tests__/Pagination.test.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import Pagination from '../Pagination';
+import userEvent from '@testing-library/user-event';
+import Pagination, { NavigationButton } from '../Pagination';
 
 const pagination = () => screen.getByTestId('test');
+const user = userEvent.setup();
 
 test('should render without crashing', () => {
 	const onChange = jest.fn();
@@ -16,4 +18,43 @@ test('should render without crashing', () => {
 	);
 
 	expect(pagination()).toBeInTheDocument();
+});
+
+test('should contain "Previous" aria-label based on directionIcon', () => {
+	const onClick = jest.fn();
+	const expected = 'Previous';
+
+	render(<NavigationButton directionIcon="prev" onClick={onClick} />);
+
+	expect(screen.getByRole('button').getAttribute('aria-label')).toContain(
+		expected
+	);
+});
+
+test('should contain "Next" aria-label based on directionIcon', () => {
+	const onClick = jest.fn();
+	const expected = 'Next';
+
+	render(<NavigationButton directionIcon="next" onClick={onClick} />);
+
+	expect(screen.getByRole('button').getAttribute('aria-label')).toContain(
+		expected
+	);
+});
+
+test('should contain a child SVG', () => {
+	const onClick = jest.fn();
+	render(<NavigationButton directionIcon="prev" onClick={onClick} />);
+
+	expect(screen.getByRole('button').firstChild?.nodeName).toBe('svg');
+});
+
+test('should call onClick', async () => {
+	const onClick = jest.fn();
+
+	render(<NavigationButton directionIcon="next" onClick={onClick} />);
+
+	await user.click(screen.getByRole('button'));
+
+	expect(onClick).toHaveBeenCalledTimes(1);
 });
